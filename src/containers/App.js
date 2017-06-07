44f615c7772265289653f95f4c558b9ec947fac1
velopert <public.velopert@gmail.com>
2017-06-08 01:35:52 +0900

    CounterContainer 컨테이너 컴포넌트 만들기

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Counter from '../components/Counter';
+
+import CounterContainer from '../containers/CounterContainer';
 class App extends Component {
     render() {
         return (
             <div>
-                <Counter/>
+                <CounterContainer/>
             </div>
         );
     }
