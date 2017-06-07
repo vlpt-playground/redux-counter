196091570aecb0b3a967b4d795018b4782bb0133
velopert <public.velopert@gmail.com>
2017-06-08 01:06:00 +0900

    Provider 로 리액트앱에 store 연동

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import './index.css';
 // Redux 관련 불러오기
 import { createStore } from 'redux'
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 // 스토어 생성
 const store = createStore(reducers);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
