62fe7529adfccee55e5863bfebc26097158469ab
velopert <public.velopert@gmail.com>
2017-06-08 03:02:10 +0900

    프리젠테이셔널 컴포넌트 만들기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
     return (
         <div 
             className="Counter" 
-            onClick={onIncrement} 
+            onClick={() => onIncrement(index)} 
             onContextMenu={
                 (e) => { 
                     e.preventDefault(); 
-                    onDecrement();
+                    onDecrement(index);
                 }
             } 
-            onDoubleClick={onSetColor}
+            onDoubleClick={() => onSetColor(index)}
             style={{backgroundColor: color}}>
                 {number}
         </div>
@@ -21,6 +21,7 @@ const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
 };
 
 Counter.propTypes = {
+    index: PropTypes.number,
     number: PropTypes.number,
     color: PropTypes.string,
     onIncrement: PropTypes.func,
@@ -29,6 +30,7 @@ Counter.propTypes = {
 };
 
 Counter.defaultProps = {
+    index: 0,
     number: 0,
     color: 'black',
     onIncrement: () => console.warn('onIncrement not defined'),
