085517fab3637a0e66d5f039fe77bca2bb5d636a
velopert <public.velopert@gmail.com>
2017-06-08 00:35:54 +0900

    Counter 컴포넌트 만들기

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Counter from '../components/Counter';
 class App extends Component {
     render() {
         return (
             <div>
-                Counter
+                <Counter/>
             </div>
         );
     }
