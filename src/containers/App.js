082310a68f79f30b54d5360c4a92cf3c82f0441f
velopert <public.velopert@gmail.com>
2017-06-08 03:09:20 +0900

    컨테이너 컴포넌트 만들기

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Buttons from '../components/Buttons';
+import CounterListContainer from './CounterListContainer';
 
-import CounterContainer from '../containers/CounterContainer';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import { getRandomColor } from '../utils';
+
 class App extends Component {
     render() {
+        const { onCreate, onRemove } = this.props;
         return (
-            <div>
-                <CounterContainer/>
+            <div className="App">
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                />
+                <CounterListContainer/>
             </div>
         );
     }
 }
 
-export default App;
+// 액션함수 준비
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: (index) => dispatch(actions.remove(index))
+});
+
+// 리덕스에 연결을 시키고 내보낸다
+export default connect(null, mapToDispatch)(App);
--- src/containers/CounterListContainer.js
+++ src/containers/CounterListContainer.js
@@ -1,34 +1,31 @@
-import Counter from '../components/Counter';
+import CounterList from '../components/CounterList';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { getRandomColor } from '../utils';
 
 // store 안의 state 값을 props 로 연결해줍니다.
 const mapStateToProps = (state) => ({
-    color: state.colorData.color,
-    number: state.numberData.number
+    counters: state.counters
 });
-
 
 /* 
     액션 생성자를 사용하여 액션을 생성하고,
     해당 액션을 dispatch 하는 함수를 만들은 후, 이를 props 로 연결해줍니다.
 */
-const mapDispatchToProps = (dispatch) => ({
-    onIncrement: () => dispatch(actions.increment()),
-    onDecrement: () => dispatch(actions.decrement()),
-    onSetColor: () => {
-        const color = getRandomColor();
-        dispatch(actions.setColor(color));
-    }
-});
 
-// Counter 컴포넌트의 Container 컴포넌트
-// Counter 컴포넌트를 어플리케이션의 데이터 레이어와 묶는 역할을 합니다.
-const CounterContainer = connect(
+const mapDispatchToProps = (dispatch) => ({
+    onIncrement: (index) => dispatch(actions.increment(index)),
+    onDecrement: (index) => dispatch(actions.decrement(index)),
+    onSetColor: (index) => {
+        const color = getRandomColor();
+        dispatch(actions.setColor({ index, color}));
+    }
+})
+
+// 데이터와 함수들이 props 로 붙은 컴포넌트 생성
+const CounterListContainer = connect(
     mapStateToProps,
     mapDispatchToProps
-)(Counter);
+)(CounterList);
 
-
-export default CounterContainer;
+export default CounterListContainer;
