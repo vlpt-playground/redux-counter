e8504997aa6ae0efb1d6ba06cb7845096cf28fd7
velopert <public.velopert@gmail.com>
2017-06-08 01:02:20 +0900

    Store 만들기

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux'
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
